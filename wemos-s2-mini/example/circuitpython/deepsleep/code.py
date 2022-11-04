3e09c6932bae7190a5f81d91075d11209aea0431
iotool <roha.mbox@gmail.com>
2022-11-04 14:04:04 +0100

    adjust to 10s periode

--- wemos-s2-mini/example/circuitpython/deepsleep/code.py
+++ wemos-s2-mini/example/circuitpython/deepsleep/code.py
@@ -1,6 +1,6 @@
-# boot   38mA  171mW  1200ms - execute bootloader
+# boot   38mA  171mW  1262ms - execute bootloader
 # code   26mA  117mW  1000ms - execute program or usb-connected
-# sleep  58uA  261uW    20s  - deep sleep, if not usb-connected
+# sleep  58uA  261uW  8938ms  - deep sleep, if not usb-connected
 
 import time
 import board
@@ -15,10 +15,10 @@ print("on")
 time.sleep(0.5)
 led.value = False
 print("off")
-time.sleep(2.5)
+time.sleep(0.5)
 
-# start wakeup after 20 seconds or if button d0 pressed
-time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 20)
+# periode 10s = wakeup after 8.938 seconds or if button d0 pressed
+time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 8.938)
 pin_alarm = alarm.pin.PinAlarm(pin=board.BUTTON, value=False, pull=True)
 # exit the program, and then deep sleep until the alarm wakes us.
 alarm.exit_and_deep_sleep_until_alarms(time_alarm, pin_alarm)

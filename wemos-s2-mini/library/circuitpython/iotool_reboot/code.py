119b7bed923c6e355ec1ee90d2ec58f1e2cea481
iotool <roha.mbox@gmail.com>
2022-11-08 10:53:08 +0100

    bugfix clear uptime

--- wemos-s2-mini/library/circuitpython/iotool_reboot/code.py
+++ wemos-s2-mini/library/circuitpython/iotool_reboot/code.py
@@ -1,43 +1,22 @@
 import time
-import alarm
 import board
 import digitalio
-import analogio
-import microcontroller
-import math
 import iotool_reboot
 
-print("Wemos-S2-mini")
+iotool_reboot.debug = True
+iotool_reboot.start(5)
+print("iotool_reboot.boot_reason: ",iotool_reboot.boot_reason)
 
-# boot reason
-iotoolReboot = iotool_reboot.IotoolReboot(0)
-
-# boot 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-if iotoolReboot.bootreason == iotool_reboot.RESETBUTTON:
-  # led blinking fast 2500ms
-  i=25
-  while i>0:
-    i=i-1
-    led.value = True
-    time.sleep(0.05)
-    led.value = False
-    time.sleep(0.05)
-elif iotoolReboot.bootreason == iotool_reboot.BOOTBUTTON:
-  # led on 2500ms
-  led.value = True
-  time.sleep(2.4)
+for i in range(iotool_reboot.boot_reason):
   led.value = False
-  time.sleep(0.1)
-elif iotoolReboot.bootreason == iotool_reboot.WAKEUPTIMER:
-  # led on 10ms
+  time.sleep(0.2)
   led.value = True
-  time.sleep(0.01)
-  led.value = False
   time.sleep(0.1)
-led.deinit()
+  led.value = False
+  time.sleep(0.2)
+time.sleep(1)
 
-# deep sleep
-iotoolReboot.deepsleep(8.938)
+iotool_reboot.deepsleep(8.938)

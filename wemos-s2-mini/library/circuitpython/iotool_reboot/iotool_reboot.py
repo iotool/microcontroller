4e5060d5362405773edbbd56c73a29fcb9ae399e
iotool <roha.mbox@gmail.com>
2022-11-08 10:52:13 +0100

    bugfix clear uptime

--- wemos-s2-mini/library/circuitpython/iotool_reboot/iotool_reboot.py
+++ wemos-s2-mini/library/circuitpython/iotool_reboot/iotool_reboot.py
@@ -2,57 +2,79 @@ import alarm
 import time
 import math
 import board
+import storage
 
+UNKNOWN = 0
 RESETBUTTON = 1
 BOOTBUTTON = 2
 WAKEUPTIMER = 3
 
-class IotoolReboot:
+debug = False
+memory_offset = 0
+boot_reason = UNKNOWN
 
-  pLoopHi = 4
-  pLoopLo = 5
+def start(iMemOffset):
+  global boot_reason, memory_offset
+  memory_offset = iMemOffset
+  # index of sleep_memory
+  iLastHi = memory_offset+0
+  iLastLo = memory_offset+1
+  iWaitHi = memory_offset+2
+  iWaitLo = memory_offset+3
+  # restore prior timestamp and periode
+  tLast = alarm.sleep_memory[iLastHi]*256+alarm.sleep_memory[iLastLo]
+  tWait = alarm.sleep_memory[iWaitHi]*256+alarm.sleep_memory[iWaitLo]
+  # current timestamp
+  tNow = time.time() % 65536
+  # calculate sleep time
+  if tLast <= tNow:
+    tSleep = tNow-tLast
+  else:
+    tSleep = tNow+(65536-tLast)
+  if debug:
+    print("iotool_reboot: init mempos",memory_offset)
+    print("iotool_reboot: init now",tNow)
+    print("iotool_reboot: init last",tLast)
+    print("iotool_reboot: init wait",tWait)
+    print("iotool_reboot: init sleep",tSleep)
+  # boot reason
+  if not alarm.wake_alarm:
+    boot_reason = RESETBUTTON
+    if debug:
+      print("iotool_reboot: init resetbutton")
+  elif tSleep < tWait:
+    boot_reason = BOOTBUTTON
+    if debug:
+      print("iotool_reboot: init bootbutton")
+  else:
+    boot_reason = WAKEUPTIMER
+    if debug:
+      print("iotool_reboot: init wakeuptimer")
 
-  def __init__(self, mempos):
-    # memory position
-    self.pFlagHi = mempos+0
-    self.pFlagLo = mempos+1
-    self.pLastHi = mempos+2
-    self.pLastLo = mempos+3
-    self.pLoopHi = mempos+4
-    self.pLoopLo = mempos+5
-    # sleep time
-    tLast = alarm.sleep_memory[self.pLastHi]*256+alarm.sleep_memory[self.pLastLo]
-    tLoop = alarm.sleep_memory[self.pLoopHi]*256+alarm.sleep_memory[self.pLoopLo]
-    tNow = math.ceil(time.monotonic()*10) % 655536
-    tNowLo = tNow % 256
-    tNowHi = math.ceil((tNow-tNowLo)/256)
-    alarm.sleep_memory[self.pLastHi] = tNowHi
-    alarm.sleep_memory[self.pLastLo] = tNowLo
-    if tLast <= tNow:
-      tSleep = tNow-tLast
-    else:
-      tSleep = tNow+(655536-tLast)
-    # boot reason
-    if not alarm.wake_alarm:
-      self.bootreason = RESETBUTTON
-      print("iotool_reboot: resetbutton")
-    elif tSleep < tLoop:
-      self.bootreason = BOOTBUTTON
-      print("iotool_reboot: bootbutton")
-    else:
-      self.bootreason = WAKEUPTIMER
-      print("iotool_reboot: wakeuptimer")
-
-  @classmethod
-  def deepsleep(self, tNext):
-    print("iotool_reboot: sleep",tNext,"s")
-    # periode 8.938 = 10s
-    tLoop = math.ceil(tNext*10) % 655536
-    tLoopLo = tLoop % 256
-    tLoopHi = math.ceil((tLoop-tLoopLo)/256)
-    alarm.sleep_memory[self.pLoopHi] = tLoopHi
-    alarm.sleep_memory[self.pLoopLo] = tLoopLo
-    # sleep
-    aTime = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + tNext)
-    aPin = alarm.pin.PinAlarm(pin=board.BUTTON, value=False, pull=True)
-    alarm.exit_and_deep_sleep_until_alarms(aTime, aPin)
+def deepsleep(tSleep):
+  # index of sleep_memory
+  iLastHi = memory_offset+0
+  iLastLo = memory_offset+1
+  iWaitHi = memory_offset+2
+  iWaitLo = memory_offset+3
+  # store last timestamp
+  tNow = time.time() % 65536
+  tNowLo = tNow % 256
+  tNowHi = tNow >> 8
+  alarm.sleep_memory[iLastHi] = tNowHi
+  alarm.sleep_memory[iLastLo] = tNowLo
+  # store sleep periode (8.938 = 10s)
+  tWait = math.ceil(tSleep) % 65536
+  tWaitLo = tWait % 256
+  tWaitHi = tWait >> 8
+  alarm.sleep_memory[iWaitHi] = tWaitHi
+  alarm.sleep_memory[iWaitLo] = tWaitLo
+  # debug
+  if debug:
+    print("iotool_reboot: exit mempos",memory_offset)
+    print("iotool_reboot: exit sleep",tSleep,"s")
+    print("iotool_reboot: exit wait",tWait,"s")
+  # deep sleep
+  aTime = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + tSleep)
+  aPin = alarm.pin.PinAlarm(pin=board.BUTTON, value=False, pull=True)
+  alarm.exit_and_deep_sleep_until_alarms(aTime, aPin)
